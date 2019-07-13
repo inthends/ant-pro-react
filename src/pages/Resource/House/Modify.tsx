55ee55ea10e067e22b1122c00ced0a1832e50af7
shenhaoxiang <hero_1401@163.com>
2019-07-13 15:50:29 +0800

    修改日期选择出错的问题

--- src/pages/Resource/House/Modify.tsx
+++ src/pages/Resource/House/Modify.tsx
@@ -18,6 +18,8 @@ import React, { useEffect, useState } from 'react';
 import { GetFormInfoJson, GetProjectType, GetTreeAreaJson, SaveForm } from './House.service';
 import styles from './style.less';
 
+import * as moment from 'moment';
+
 const { Option } = Select;
 const { TextArea } = Input;
 const { TreeNode } = Tree;
@@ -40,6 +42,8 @@ const Modify = (props: ModifyProps) => {
   const [city, setCity] = useState<TreeEntity[]>([]);
   const [area, setArea] = useState<TreeEntity[]>([]);
   const [project, setProject] = useState<TreeEntity[]>([]);
+
+  const [infoDetail, setInfoDetail] = useState<any>({});
 
   // 打开抽屉时初始化
   useEffect(() => {
@@ -72,14 +76,15 @@ const Modify = (props: ModifyProps) => {
   useEffect(() => {
     if (modifyVisible) {
       if (id) {
-        getInfo(id).then(tempInfo => {
+        getInfo(id).then((tempInfo: any) => {
           if (tempInfo.province) {
             getCity(tempInfo.province, true);
           }
           if (tempInfo.city) {
             getArea(tempInfo.city, true);
           }
-          form.setFieldsValue(tempInfo);
+          setInfoDetail(tempInfo);
+          form.resetFields();
         });
       } else {
         form.setFieldsValue({ organizeId });
@@ -96,7 +101,8 @@ const Modify = (props: ModifyProps) => {
     form.validateFields((errors, values) => {
       if (!errors) {
         getInfo(id).then(tempInfo => {
-          SaveForm({ ...tempInfo, ...values, keyValue: tempInfo.pStructId }).then(res => {
+          let newvalue = { ...values, date: values.date.format('YYYY-MM-DD') };
+          SaveForm({ ...tempInfo, ...newvalue, keyValue: tempInfo.pStructId }).then(res => {
             message.success('保存成功');
             closeDrawer();
             reload();
@@ -108,13 +114,14 @@ const Modify = (props: ModifyProps) => {
   const getInfo = id => {
     if (id) {
       return GetFormInfoJson(id).then(res => {
-        const { baseInfo, pProperty } = res || {};
+        const { baseInfo, pProperty } = res || ({} as any);
         let info = {
           ...pProperty,
           ...baseInfo,
         };
         info.id = pProperty && pProperty.id;
         info.pStructId = baseInfo && baseInfo.id;
+        info.area = pProperty.area;
         return info;
       });
     } else {
@@ -135,198 +142,254 @@ const Modify = (props: ModifyProps) => {
       bodyStyle={{ background: '#f6f7fb', minHeight: 'calc(100% - 55px)' }}
     >
       <Card title="基本信息" className={styles.card} bordered={false}>
-        <Form layout="vertical" hideRequiredMark>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="隶属机构" required>
-                {getFieldDecorator('organizeId', {
-                  rules: [{ required: true, message: '请选择隶属机构' }],
-                })(
-                  <TreeSelect placeholder="请选择隶属机构" allowClear treeDefaultExpandAll>
-                    {renderTree(treeData, '0')}
-                  </TreeSelect>,
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
+        {modifyVisible ? (
+          <Form layout="vertical" hideRequiredMark>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="隶属机构" required>
+                  {getFieldDecorator('organizeId', {
+                    initialValue: infoDetail.organizeId,
+                    rules: [{ required: true, message: '请选择隶属机构' }],
+                  })(
+                    <TreeSelect placeholder="请选择隶属机构" allowClear treeDefaultExpandAll>
+                      {renderTree(treeData, '0')}
+                    </TreeSelect>,
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
 
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="小区编号" required>
-                {getFieldDecorator('code', {
-                  rules: [{ required: true, message: '请输入小区编号' }],
-                })(<Input placeholder="请输入小区编号" />)}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="小区名称" required>
-                {getFieldDecorator('name', {
-                  rules: [{ required: true, message: '请输入小区名称' }],
-                })(<Input placeholder="请输入小区名称" />)}
-              </Form.Item>
-            </Col>
-          </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="小区编号" required>
+                  {getFieldDecorator('code', {
+                    initialValue: infoDetail.code,
+                    rules: [{ required: true, message: '请输入小区编号' }],
+                  })(<Input placeholder="请输入小区编号" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="小区名称" required>
+                  {getFieldDecorator('name', {
+                    initialValue: infoDetail.name,
+                    rules: [{ required: true, message: '请输入小区名称' }],
+                  })(<Input placeholder="请输入小区名称" />)}
+                </Form.Item>
+              </Col>
+            </Row>
 
-          <Row gutter={24}>
-            <Col lg={24}>
-              <Row gutter={24}>
-                <Col lg={8}>
-                  <Form.Item label="所属省">
-                    {getFieldDecorator('province', {})(
-                      <Select
-                        showSearch
-                        onChange={getCity}
-                        filterOption={(input, option) => option.props.children.indexOf(input) >= 0}
-                      >
-                        {pro.map(item => (
-                          <Option key={item.value} value={item.value}>
-                            {item.text}
-                          </Option>
-                        ))}
-                      </Select>,
-                    )}
-                  </Form.Item>
-                </Col>
-                <Col lg={8}>
-                  <Form.Item label="所属市">
-                    {getFieldDecorator('city', {})(
-                      <Select
-                        showSearch
-                        onChange={getArea}
-                        filterOption={(input, option) => option.props.children.indexOf(input) >= 0}
-                      >
-                        {city.map(item => (
-                          <Option key={item.value} value={item.value}>
-                            {item.text}
-                          </Option>
-                        ))}
-                      </Select>,
-                    )}
-                  </Form.Item>
-                </Col>
-                <Col lg={8}>
-                  <Form.Item label="所属区/县">
-                    {getFieldDecorator('region', {})(
-                      <Select
-                        showSearch
-                        filterOption={(input, option) => option.props.children.indexOf(input) >= 0}
-                      >
-                        {area.map(item => (
-                          <Option key={item.value} value={item.value}>
-                            {item.text}
-                          </Option>
-                        ))}
-                      </Select>,
-                    )}
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={24}>
-              <Form.Item label="详细地址">
-                {getFieldDecorator('address', {})(<Input placeholder="请输入详细地址" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="环线">
-                {getFieldDecorator('cicleLine', {})(<Input placeholder="请输入环线" />)}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="地铁">
-                {getFieldDecorator('metro', {})(<Input placeholder="请输入地铁" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="经度">
-                {getFieldDecorator('lat', {})(<Input placeholder="请输入经度" />)}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="纬度">
-                {getFieldDecorator('lng', {})(<Input placeholder="请输入纬度" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="总建筑面积(㎡)">
-                {getFieldDecorator('area', {})(<Input placeholder="请输入总建筑面积" />)}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="占地面积(㎡)">
-                {getFieldDecorator('coverArea', {})(<Input placeholder="请输入占地面积" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="容积率(㎡)">
-                {getFieldDecorator('volumeRate', {})(<Input placeholder="请输入容积率" />)}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="绿化面积(㎡)">
-                {getFieldDecorator('greenRate', {})(<Input placeholder="请输入绿化面积" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="项目类型">
-                {getFieldDecorator('propertyType', {})(
-                  <Select>
-                    {project.map(item => (
-                      <Option key={item.value} value={item.value}>
-                        {item.text}
-                      </Option>
-                    ))}
-                  </Select>,
-                )}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="建成年份">
-                {getFieldDecorator('createYear', {})(<DatePicker placeholder="请输入建成年份" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={12}>
-              <Form.Item label="开发商">
-                {getFieldDecorator('developerName', {})(<Input placeholder="请输入开发商" />)}
-              </Form.Item>
-            </Col>
-            <Col lg={12}>
-              <Form.Item label="物业公司">
-                {getFieldDecorator('propertyCompany', {})(<Input placeholder="请输入物业公司" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={24}>
-              <Form.Item label="物业标准费">
-                {getFieldDecorator('feeItemRule', {})(<Input placeholder="请输入物业标准费" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={24}>
-            <Col lg={24}>
-              <Form.Item label="附加说明">
-                {getFieldDecorator('memo', {})(
-                  <TextArea rows={4} placeholder="请输入物业标准费" />,
-                )}
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
+            <Row gutter={24}>
+              <Col lg={24}>
+                <Row gutter={24}>
+                  <Col lg={8}>
+                    <Form.Item label="所属省">
+                      {getFieldDecorator('province', {
+                        initialValue: infoDetail.province,
+                      })(
+                        <Select
+                          showSearch
+                          onChange={getCity}
+                          filterOption={(input, option) =>
+                            option.props.children.indexOf(input) >= 0
+                          }
+                        >
+                          {pro.map(item => (
+                            <Option key={item.value} value={item.value}>
+                              {item.text}
+                            </Option>
+                          ))}
+                        </Select>,
+                      )}
+                    </Form.Item>
+                  </Col>
+                  <Col lg={8}>
+                    <Form.Item label="所属市">
+                      {getFieldDecorator('city', {
+                        initialValue: infoDetail.city,
+                      })(
+                        <Select
+                          showSearch
+                          onChange={getArea}
+                          filterOption={(input, option) =>
+                            option.props.children.indexOf(input) >= 0
+                          }
+                        >
+                          {city.map(item => (
+                            <Option key={item.value} value={item.value}>
+                              {item.text}
+                            </Option>
+                          ))}
+                        </Select>,
+                      )}
+                    </Form.Item>
+                  </Col>
+                  <Col lg={8}>
+                    <Form.Item label="所属区/县">
+                      {getFieldDecorator('region', {
+                        initialValue: infoDetail.region,
+                      })(
+                        <Select
+                          showSearch
+                          filterOption={(input, option) =>
+                            option.props.children.indexOf(input) >= 0
+                          }
+                        >
+                          {area.map(item => (
+                            <Option key={item.value} value={item.value}>
+                              {item.text}
+                            </Option>
+                          ))}
+                        </Select>,
+                      )}
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={24}>
+                <Form.Item label="详细地址">
+                  {getFieldDecorator('address', {
+                    initialValue: infoDetail.address,
+                  })(<Input placeholder="请输入详细地址" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="环线">
+                  {getFieldDecorator('cicleLine', {
+                    initialValue: infoDetail.cicleLine,
+                  })(<Input placeholder="请输入环线" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="地铁">
+                  {getFieldDecorator('metro', {
+                    initialValue: infoDetail.metro,
+                  })(<Input placeholder="请输入地铁" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="经度">
+                  {getFieldDecorator('lat', {
+                    initialValue: infoDetail.lat,
+                  })(<Input placeholder="请输入经度" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="纬度">
+                  {getFieldDecorator('lng', {
+                    initialValue: infoDetail.lng,
+                  })(<Input placeholder="请输入纬度" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="总建筑面积(㎡)">
+                  {console.log(infoDetail.area)}
+                  {getFieldDecorator('area', {
+                    initialValue: infoDetail.area || 0,
+                  })(<Input placeholder="请输入总建筑面积" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="占地面积(㎡)">
+                  {getFieldDecorator('coverArea', {
+                    initialValue: infoDetail.coverArea || 0,
+                  })(<Input placeholder="请输入占地面积" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="容积率">
+                  {getFieldDecorator('volumeRate', {
+                    initialValue: infoDetail.volumeRate || 0,
+                  })(<Input placeholder="请输入容积率" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="绿化面积(㎡)">
+                  {getFieldDecorator('greenRate', {
+                    initialValue: infoDetail.greenRate || 0,
+                  })(<Input placeholder="请输入绿化面积" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="项目类型">
+                  {getFieldDecorator('propertyType', {
+                    initialValue: infoDetail.propertyType,
+                  })(
+                    <Select placeholder="请选择项目类型">
+                      {project.map(item => (
+                        <Option key={item.value} value={item.value}>
+                          {item.text}
+                        </Option>
+                      ))}
+                    </Select>,
+                  )}
+                </Form.Item>
+              </Col>
+
+              <Col lg={12}>
+                <Form.Item label="接盘日期">
+                  {getFieldDecorator('date', {
+                    initialValue: infoDetail.date
+                      ? moment(new Date(infoDetail.date))
+                      : moment(new Date()),
+                  })(<DatePicker />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="开发商">
+                  {getFieldDecorator('developerName', {
+                    initialValue: infoDetail.developerName,
+                  })(<Input placeholder="请输入开发商" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="建成年份">
+                  {getFieldDecorator('createYear', { initialValue: infoDetail.createYear })(
+                    <Input placeholder="请输入建成年份" />,
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={12}>
+                <Form.Item label="物业公司">
+                  {getFieldDecorator('propertyCompany', {
+                    initialValue: infoDetail.propertyCompany,
+                  })(<Input placeholder="请输入物业公司" />)}
+                </Form.Item>
+              </Col>
+              <Col lg={12}>
+                <Form.Item label="物业标准费">
+                  {getFieldDecorator('feeItemRule', {
+                    initialValue: infoDetail.feeItemRule,
+                  })(<Input placeholder="请输入物业标准费" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={24}>
+              <Col lg={24}>
+                <Form.Item label="附加说明">
+                  {getFieldDecorator('memo', {
+                    initialValue: infoDetail.memo,
+                  })(<TextArea rows={4} placeholder="请输入附加说明" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        ) : null}
       </Card>
       <div
         style={{
