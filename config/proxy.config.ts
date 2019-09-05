ae8d8f9092f86eca9cafde0d3df974bc4b8de91c
inthend <87696974@qq.com>
2019-09-05 09:52:20 +0800

    test

--- config/proxy.config.ts
+++ config/proxy.config.ts
@@ -8,15 +8,7 @@ export interface Proxy {
 
 export default {
   '/api': {
-<<<<<<< HEAD
-     target: 'http://hf.jslesoft.com:8018',
-     //target:'http://localhost:52520',
-
-
-
-=======
-     //target: 'http://hf.jslesoft.com:8018', 
+     //target: 'http://hf.jslesoft.com:8018',
      target:'http://localhost:52520', 
->>>>>>> a0f198cb27e8a934348854e45404ae68ab753cd0
   },
 } as Proxy;
