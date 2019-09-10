7addd90f6018f80605256e91486b59ac5f79dd4f
inthend <87696974@qq.com>
2019-09-10 13:50:05 +0800

    删除小写重名目录

--- src/pages/register/Register.service.tsx
+++
@@ -1,8 +0,0 @@
-import { BaseRespBean } from '@/model/models';
-import request from '@/utils/request';
-
-export const register = (data): Promise<BaseRespBean> => {
-  return request.post(process.env.basePath + '/login/signUp', {
-    data,
-  });
-};
