c651ba66d0b0199a6fd9df60d83c1285d5f35e12
xkwg <xkwg@163.com>
2019-09-15 17:10:06 +0800

    提交代码

--- src/pages/System/Role/ChooseUser.tsx
+++ src/pages/System/Role/ChooseUser.tsx
@@ -1,6 +1,8 @@
-import { Modal, Tree, Input } from "antd";
-import React, { useEffect, useState } from "react";
-import { getRoleTree, getUserList } from "./Role.service";
+import { Modal, Tree, Input, Row, Col } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { getRoleTree, getUserList } from './Role.service';
+import UserLabel from './UserLabel';
+
 const { Search } = Input;
 interface ModifyProps {
   visible: boolean;
@@ -12,23 +14,22 @@ const ChooseUser = (props: ModifyProps) => {
   const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
   const [treeData, setTreeData] = useState<any[]>([]);
 
-  const [search, setSearch] = useState({
-    departmentId: ""
-  });
+  const [searchText, setSearchText] = useState<string>('');
+  const [userLIst, setUserLIst] = useState<any[]>([]);
 
   useEffect(() => {
     if (visible) {
       getRoleTree().then(res => {
         setTreeData(res);
       });
-      searchUser({ departmentId: "" });
+      searchUser({ departmentId: '' });
     }
   }, [visible]);
 
   const searchUser = ({ departmentId }) => {
     const { roleId } = data;
     getUserList({ roleId, departmentId }).then(res => {
-      console.log(res);
+      setUserLIst(JSON.parse(res) || []);
     });
   };
 
@@ -45,40 +46,46 @@ const ChooseUser = (props: ModifyProps) => {
       width={800}
       bodyStyle={{ padding: 0 }}
     >
-      <div style={{ height: 500, display: "flex" }}>
+      <div style={{ height: 500, display: 'flex' }}>
         <div
           style={{
             width: 250,
             padding: 24,
-            height: "100%",
-            overflow: "auto",
-            borderRight: "1px solid #e8e8e8"
+            height: '100%',
+            overflow: 'auto',
+            borderRight: '1px solid #e8e8e8',
           }}
         >
           <Tree
-            checkable={true}
             showLine
             treeData={treeData}
             expandedKeys={expandedKeys}
             onExpand={clickExpend}
             onSelect={e => {
-              console.log(e);
+              searchUser({ departmentId: e });
             }}
           ></Tree>
         </div>
         <div
           style={{
             padding: 24,
-            flexGrow: 1
+            flexGrow: 1,
           }}
         >
           <div>
-            {/* <Search
+            <Search
               className="search-input"
               placeholder="请输入要查询的关键词"
-              onSearch={keyword => loadData({ ...search, keyword })}
-              style={{ width: 200 }}
-            /> */}
+              onSearch={setSearchText}
+              style={{ width: 240 }}
+            />
+          </div>
+          <div style={{ paddingTop: 24 }}>
+            <Row gutter={16}>
+              {userLIst.map(item => {
+                return <UserLabel {...item} key={item.Id} />;
+              })}
+            </Row>
           </div>
         </div>
       </div>
