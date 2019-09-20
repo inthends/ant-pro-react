ac42c13b5d46f9a03a68dcbe95beb70281ae89b8
inthend <87696974@qq.com>
2019-09-20 14:30:52 +0800

    接口异常待处理

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,13 @@
 import { getResult } from '@/utils/networkUtils';
 import request from '@/utils/request';
 
+//查询模块
 export async function query(): Promise<any[]> {
   return request
     .get(process.env.basePath + '/Authorize/GetModuleList')
     .then(getResult);
 }
+//查询按钮
 export async function queryBtn(): Promise<any[]> {
   return request
     .get(process.env.basePath + '/Authorize/GetModuleButtonList')
